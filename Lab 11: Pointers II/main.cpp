c9d6585991152b88cb80c6d152ab56a061a344a3
Guo An Wang <you@example.com>
2025-09-18 23:18:29 -0700

    Initial commit

---
+++ Lab 11: Pointers II/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMSC-210 | Lab 11: Pointers II| Guo An Wang
+//  IDE used: Xcode
+//  Created by Guo An Wang on 9/18/25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
